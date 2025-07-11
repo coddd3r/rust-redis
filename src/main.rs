3f970caad6f374382a29e3c52d1da53c86de19ba
Dario Didi <dididario.dd2@gmail.com>
2025-07-11 18:58:56 +0300

    respond to a single connection while there are lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::io::Write;
+use std::io::{prelude::*, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -19,5 +19,10 @@ fn main() {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    stream.write_all(b"+PONG\r\n").unwrap();
+    // stream.write_all(b"+PONG\r\n").unwrap();
+    let buf_reader = BufReader::new(&stream);
+    let _ = buf_reader
+        .lines()
+        .map(|result| stream.write_all(b"+PONG\r\n").unwrap())
+        .take_while(|line| !line.is_empty());
 }
