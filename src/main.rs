a20d390b09a95bb209fc5286787e707156f03f60
Dario Didi <dididario.dd2@gmail.com>
2025-07-18 23:47:57 +0300

    move bulk string processing to function

--- src/main.rs
+++ src/main.rs
@@ -33,17 +33,9 @@ fn main() {
 
 fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     loop {
-        let reader = BufReader::new(&stream);
-        let mut all_lines = Vec::new();
-        let mut my_iter = reader.lines();
-        let Some(arr_length) = my_iter.next() else {
+        let Some(all_lines) = decode_bulk_string(&stream) else {
             break;
         };
-        let arr_length = arr_length.unwrap()[1..].parse::<usize>().unwrap() * 2;
-        eprintln!("length: {arr_length}");
-        for _ in 0..arr_length {
-            all_lines.push(my_iter.next().unwrap().unwrap());
-        }
         eprintln!("ALL LINES:{:?}", all_lines);
 
         let cmd = &all_lines[1];
@@ -56,7 +48,7 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
                 let resp = [b"+", all_lines[3].as_bytes(), b"\r\n"].concat();
                 stream.write_all(&resp).unwrap()
             }
-            i => eprintln!("UNEXPECTED INPUT {i}"),
+            _ => unreachable!(),
         }
     }
     Ok(())
@@ -64,8 +56,16 @@ fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
 
 //https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
 //The exact bytes your program will receive won't be just ECHO hey, you'll receive something like this: *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n. That's ["ECHO", "hey"] encoded using the Redis protocol.
-//fn decode_bulk_string(val: &[u8]) -> String {
-//    let msg_size = String::from_utf8_lossy(&[val[5]]).to_string();
-//    let command = String::from_utf8_lossy(&val[8..12]);
-//    return command.to_string();
-//}
+fn decode_bulk_string(stream: &TcpStream) -> Option<Vec<String>> {
+    let mut all_lines = Vec::new();
+    let mut my_iter = BufReader::new(stream).lines();
+    //if next returns None then no more lines, break loop, free thread
+    let arr_length = my_iter.next()?;
+    //for each element we'll have 2 lines, one with the size and the other with the text
+    //so arr_length will ne provided num of elements * 2
+    let arr_length = arr_length.unwrap()[1..].parse::<usize>().unwrap() * 2;
+    for _ in 0..arr_length {
+        all_lines.push(my_iter.next()?.unwrap());
+    }
+    Some(all_lines)
+}
