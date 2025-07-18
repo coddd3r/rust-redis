4d46d0b0285bae6ecf0c5c2d9fe34a9d8c80b397
Dario Didi <dididario.dd2@gmail.com>
2025-07-18 18:51:14 +0300

    add concurrency support with threadpool

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,30 @@ use std::error::Error;
 use std::io::{prelude::*, BufReader, BufWriter, Write};
 use std::net::{TcpListener, TcpStream};
 
+use codecrafters_redis::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let stream_pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
-                handle_client(_stream).unwrap();
+                stream_pool.execute(|| {
+                    let res = handle_client(_stream);
+                    match res {
+                        Ok(_) => (),
+                        Err(e) => eprintln!("Error handling client {}", e),
+                    }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    println!("Shutting down.");
 }
 
 fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
