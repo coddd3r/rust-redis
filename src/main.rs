2e2590caf3c1c8b649dc8842565c742e95b739df
Dario Didi <dididario.dd2@gmail.com>
2025-07-19 19:34:44 +0300

    format response as array

--- src/main.rs
+++ src/main.rs
@@ -124,12 +124,29 @@ fn handle_client(
                 match config_command.as_str() {
                     "get" => match config_field.as_str() {
                         "dir" => {
-                            let resp = [b"+", dir.as_ref().unwrap().as_bytes(), b"\r\n"].concat();
+                            let dir_name = dir.as_ref().unwrap();
+                            let dir_name_length = dir_name.len().to_string();
+                            let resp = [
+                                b"*2\r\n$3\r\ndir\r\n",
+                                dir_name_length.as_bytes(),
+                                b"\r\n",
+                                dir_name.as_bytes(),
+                                b"\r\n",
+                            ]
+                            .concat();
                             stream.write_all(&resp).unwrap();
                         }
                         "dbfilename" => {
-                            let resp = [b"+", (db_filename.as_ref()).unwrap().as_bytes(), b"\r\n"]
-                                .concat();
+                            let db_name = db_filename.as_ref().unwrap();
+                            let db_name_length = db_name.len().to_string();
+                            let resp = [
+                                b"*2\r\n$3\r\ndir\r\n",
+                                db_name_length.as_bytes(),
+                                b"+",
+                                db_name.as_bytes(),
+                                b"\r\n",
+                            ]
+                            .concat();
                             stream.write_all(&resp).unwrap();
                         }
                         _ => unreachable!(),
@@ -143,13 +160,19 @@ fn handle_client(
     Ok(())
 }
 
-//https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
-//The exact bytes your program will receive won't be just ECHO hey, you'll receive something like this: *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n. That's ["ECHO", "hey"] encoded using the Redis protocol.
+/**
+*
+*   https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+    /The exact bytes your program will receive won't be just ECHO hey, you'll receive something like this: *2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n. That's ["ECHO", "hey"] encoded using the Redis protocol.
+*
+**/
 fn decode_bulk_string(stream: &TcpStream) -> Option<Vec<String>> {
     let mut all_lines = Vec::new();
     let mut my_iter = BufReader::new(stream).lines();
+
     //if next returns None then no more lines, break loop, free thread
     let arr_length = my_iter.next()?;
+
     //for each element we'll have 2 lines, one with the size and the other with the text
     //so arr_length will ne provided num of elements * 2
     let arr_length = arr_length.unwrap()[1..].parse::<usize>().unwrap() * 2;
